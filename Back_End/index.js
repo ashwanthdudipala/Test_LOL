21115b896b9b3efc976ce8fb228dfbec0c75b9cc
ashwanthdudipala <ashwanthreddydudipala@gmail.com>
2025-09-01 23:23:54 +0530

    backend_mod

--- Back_End/index.js
+++ Back_End/index.js
@@ -7,7 +7,10 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://your-frontend.onrender.com"],
+  credentials: true
+}));
 app.use(express.json());
 
 /* Controllers */
