8dd20e27bc932d5d2d9835bdb4e42f35e6430b1f
ashwanthdudipala <ashwanthreddydudipala@gmail.com>
2025-09-02 23:54:39 +0530

    trying_1

--- reactapp/src/components/PersonalDetails.jsx
+++ reactapp/src/components/PersonalDetails.jsx
@@ -18,41 +18,41 @@ export default function PersonalDetails(){
             <div className="subflex1">
                 <div className="detail-row">
                     <span className="label">First Name</span>
-                    <span className="value">{details.firstName}</span>
+                    <span className="value">{details.firstName || "N/A"}</span>
                 </div>
                 <div className="detail-row">
                     <span className="label">Last Name</span>
-                    <span className="value">{details.lastName}</span>
+                    <span className="value">{details.lastName || "N/A"}</span>
                 </div>
                 <div className="detail-row">
                     <span className="label">Age</span>
-                    <span className="value">{details.age}</span>
+                    <span className="value">{details.age || "N/A"}</span>
                 </div>
                 <div className="detail-row">
                     <span className="label">Gmail</span>
-                    <span className="value">{details.email}</span>
+                    <span className="value">{details.email || "N/A"}</span>
                 </div>
                 <div className="detail-row">
                     <span className="label">Phone</span>
-                    <span className="value">{details.phoneNum}</span>
+                    <span className="value">{details.phoneNum || "N/A"}</span>
                 </div>
             </div>
             <div className="subflex2">
                 <div className="detail-row">
                 <span className="label">Street</span>
-                <span className="value">{details.adress}</span>
+                <span className="value">{details.address || "N/A"}</span>
             </div>
             <div className="detail-row">
                 <span className="label">City</span>
-                <span className="value">{details.city}</span>
+                <span className="value">{details.city || "N/A"}</span>
             </div>
             <div className="detail-row">
                 <span className="label">State</span>
-                <span className="value">{details.state}</span>
+                <span className="value">{details.state || "N/A"}</span>
             </div>
             <div className="detail-row">
                 <span className="label">Pincode</span>
-                <span className="value">{details.pinCode}</span>
+                <span className="value">{details.pinCode || "N/A"}</span>
             </div>
             </div>
     </div>
